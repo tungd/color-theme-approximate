476b912bff8964ee1035584b656246f6c74cd467
Tung Dao <me@tungdao.com>
2013-01-14 21:29:10 +0700

    add elpa header and autoload cookie

--- color-theme-approximate.el
+++ color-theme-approximate.el
@@ -1,3 +1,60 @@
+;;; color-theme-approximate.el --- Makes Emacs theme works on terminal transparently
+;;;
+;; Author: Tung Dao <me@tungdao.com>
+;; Version: 0.1
+;;
+;; This file is NOT part of GNU Emacs
+;;
+;;; License: BSD http://opensource.org/licenses/BSD-3-Clause
+;;
+;; Copyright (c) 2013, Tung Dao
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;
+;; * Redistributions of source code must retain the above copyright notice, this
+;; list of conditions and the following disclaimer.
+;; * Redistributions in binary form must reproduce the above copyright notice,
+;; this list of conditions and the following disclaimer in the documentation
+;; and/or other materials provided with the distribution.
+;; * Neither the name of the <ORGANIZATION> nor the names of its contributors may
+;; be used to endorse or promote products derived from this software without
+;; specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
+;;
+;;
+;;; Commentary:
+;;
+;; This package advise the `load-theme' function and convert non terminal colors
+;; to their closest approximation. Inspired by VIM's CSApprox plugin
+;; http://www.vim.org/scripts/script.php?script_id=2390
+;;
+;;; Installation:
+;; Add the to your .emacs or similar:
+;;
+;; (autoload 'color-theme-approximate-on "color-theme-approximate")
+;; (color-theme-approximate-on)
+;;
+;;; TODO:
+;; - Customizable approximator
+;;
+;;; Changelog
+;;
+;; v0.1, Jan 14 2013
+;; - Initial version
+;;
 
 (require 'color)
 
@@ -73,11 +130,13 @@ Usage: (mapcar #'ca-process-component ...)"
 (defadvice load-theme (after ca-apply-approximation)
   (mapc #'ca-process-face (face-list)))
 
+;;;###autoload
 (defun color-theme-approximate-on ()
   (interactive)
   (ad-enable-advice 'load-theme 'after 'ca-apply-approximation)
   (ad-activate 'load-theme))
 
+;;;###autoload
 (defun color-theme-approximate-off ()
   (interactive)
   (ad-disable-advice 'load-theme 'after 'ca-apply-approximation)
